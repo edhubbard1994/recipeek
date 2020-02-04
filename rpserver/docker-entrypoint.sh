b2f695511b91d04414d8bb8a7ffbb244dd17c4f0
Edward Hubbard <edwardhubbard@Edwards-MacBook-Pro.local>
2020-02-04 17:46:09 -0600

    found dockerfile problem

--- rpserver/docker-entrypoint.sh
+++ rpserver/docker-entrypoint.sh
@@ -1,6 +1,12 @@
-echo "Apply database migrations"
+#!/bin/sh
+cd app
+
+echo "-----Making new migrations"
+python manage.py makemigrations
+#apply migrations
+echo "----Apply database migrations"
 python manage.py migrate
 
 # Start server
-echo "Starting server"
+echo "----Starting server"
 python manage.py runserver 0.0.0.0:8000
