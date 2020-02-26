8e2899bcc68965cde851e3747864c2f6661ae229
wrinkleintime <ssiner2@gmail.com>
2020-02-25 18:37:28 -0600

    Update database schemas

--- rpserver/recipeek/models.py
+++ rpserver/recipeek/models.py
@@ -3,14 +3,52 @@ from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
+class Recipe(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=120)
+    image_url = models.URLField()
+    recipe_url = models.URLField()
+    calories = models.IntegerField(default=0)
+    cuisine = models.ManytoManyField(Cuisine)
+    diet = models.ManytoManyField(Diet, through='RecipeIngredient')
+    user = models.ManytoManyField(User, through='UserRecipe')
 
-class RecipeModel(models.Model):
-    name = models.CharField(max_length=120)
-    meal = models.CharField(max_length=30, choices=(('breakfast','breakfast'),('lunch','lunch'),
-        ('dinner','dinner'),('brunch','brunch'),('snack','snack'),('dessert','dessert'),('other','other')),default='other') 
-    ingredients = ArrayField(models.CharField(max_length=120),size=30)
-    instructions = ArrayField(models.CharField(max_length=500), null=True)
-    tags = ArrayField(models.CharField(max_length=20),null=True)
+    def __str__(self):
+        return self.title
 
-    def __str__():
-        return "Recipe"
+class RecipeIngredient(models.Model):
+    id = models.AutoField(primary_key=True)
+    amount = models.FloatField()
+    unit = models.CharField(max_length=36)
+    recipe = models.ForeignKey(Recipe)
+    ingredient = models.ForeignKey(Ingredient)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+class Cuisine(models.Model):
+    name = models.CharField(max_length=200, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+class Diet(models.Model):
+    name = models.CharField(max_length=200, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+
+    def __str__(self):
+        return self.email
+
+class UserRecipe(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User)
+    recipe = models.ForeignKey(Recipe)
+    isFavorite = models.BooleanField(default = false)
