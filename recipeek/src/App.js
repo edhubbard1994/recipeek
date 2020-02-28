0df5405327e87da821ece9ef5aa16d7616dd2588
Maya <mgocalkappos@luc.edu>
2020-02-27 23:52:12 -0600

    Preliminary UI stuff

--- recipeek/src/App.js
+++ recipeek/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 class App extends React.Component {
 
@@ -14,16 +14,16 @@ class App extends React.Component {
   }
 
   getData = ()=>{
-  
+
     //TODO: MAKE THIS WORK WITH DNS RESOLUTION
     fetch('http://localhost:8000/api/recipe')
       .then(response => response.json())
       .then(data => this.setState({recipes: data}))
-    
+
   }
 
   render(){
-    
+
     return (
       <div className="App">
         <p>{JSON.stringify(this.state.recipes)}</p>
