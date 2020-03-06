9e9b54252e2a58c900a7d218785fa8d7ada9e5fb
Maya <mgocalkappos@luc.edu>
2020-03-05 23:04:07 -0600

    Added searchbar to homepage.

--- recipeek/src/routes/Home/Home.js
+++ recipeek/src/routes/Home/Home.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Searchbar from '../../components/Searchbar';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
                 <h1>Recipeek</h1>
+                <Searchbar
+                  label = 'Search'
+                  onClick = {() => {alert('Button Clicked')}}
+                  searchInput = ''
+                  onChange = { () => {alert('onChange')} }
+                />
             </div>
         );
     }
