936d8fc9ae7ae21fd37c809f602297fd4a5c7060
Maya <mgocalkappos@luc.edu>
2020-03-06 22:02:18 -0600

    Removed unnecessary import statements.

--- recipeek/src/App.js
+++ recipeek/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './styles/App.css';
 import Home from './routes/Home';
-import { Route, Link, Redirect } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 class App extends React.Component {
 
