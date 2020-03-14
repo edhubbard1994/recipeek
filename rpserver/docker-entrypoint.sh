87ad42843e9052ece245d8d0596c9b61c01aaa67
Edward Hubbard <edwardhubbard@Edwards-MacBook-Pro.local>
2020-03-14 01:14:54 -0500

    celery

--- rpserver/docker-entrypoint.sh
+++ rpserver/docker-entrypoint.sh
@@ -6,7 +6,6 @@ python manage.py makemigrations
 #apply migrations
 echo "----Apply database migrations"
 python manage.py migrate
-
 # Start server
 echo "----Starting server"
 python manage.py runserver 0.0.0.0:8000
--- rpserver/rpserver/urls.py
+++ rpserver/rpserver/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('recipeek.urls')),
     url(r'^api-auth/', include('rest_framework.urls')),
 ]
+
