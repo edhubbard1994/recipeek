783fd06c03014c26ef28a303a77581dcaf407c65
Edward Hubbard <edwardhubbard@Edwards-MacBook-Pro.local>
2020-03-16 22:08:41 -0500

    scheduler service

--- scraper/scheduler.py
+++ scraper/scheduler.py
@@ -2,17 +2,26 @@ import signal
 import sys
 import datetime
 
+
+class Scheduler:
+
+    def __init__(self):
+        self.
+
+
 def docker_shutdown():
     sys.exit()
+
+def main():
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        sys.exit()
+
 
 if __name__ == "__main__":
 
     signal.signal(signal.SIGTERM, docker_shutdown)
     signal.signal(signal.SIGINT, docker_shutdown)
-
-    try:
-        while True:
-            pass
-
-    except KeyboardInterrupt:
-        sys.exit()
+    main()
