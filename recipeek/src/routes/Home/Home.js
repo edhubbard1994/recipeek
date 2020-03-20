3fca47cbfd723d8c766f34e2dd92adf93d14f0af
Maya <mgocalkappos@luc.edu>
2020-03-20 00:27:36 -0500

    Implemented grid layout on homepage! 🥳

--- recipeek/src/routes/Home/Home.js
+++ recipeek/src/routes/Home/Home.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import Searchbar from '../../components/Searchbar';
+import styles from './Home.module.css';
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
+            <div className={styles.wrapper}>
+              <div className={styles.header}>
                 <h1>Recipeek</h1>
-                {/* TODO center searchbar */}
+              </div>
+              <div className={styles.search}>
                 <Searchbar
                   label = 'Find your feast...'
                   onClick = {() => {alert('Button Clicked')}}
                   searchInput = ''
                   onChange = { () => {alert('onChange')} }
                 />
+              </div>
             </div>
         );
     }
