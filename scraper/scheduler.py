b373051ea326b85312d024c9ca00830401ad91d3
Edward Hubbard <edwardhubbard@Edwards-MacBook-Pro.local>
2020-03-21 16:04:09 -0500

    json classes

--- scraper/scheduler.py
+++ scraper/scheduler.py
@@ -5,6 +5,17 @@ import schedule
 import scrape
 
 
+task_queue = []
+
+
+def run_tasks():
+    collected_data = []
+    while len(task_queue) > 0:
+        task = task_queue.pop()
+        data = task()
+        collected_data.extend(data)
+    print(collected_data)
+
 def docker_shutdown():
     #add any additional cleanup here
     sys.exit()
--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,6 +1,36 @@
 from bs4 import BeautifulSoup
 
+
+
+class Ingredient:
+    
+    def __init__(self,ingrideient, amount, unit, diet=None):
+        self.ingredient = ingrideient
+        self.amount = amount
+        self.unit = unit
+        if diet:
+            self.diet = diet        
+
 class Recipe:
 
-    def __init__(self,title,):
-        self.title
+    def __init__(self,title,recipe_url,cuisine,ingredients,image_url=None,calories=None):
+        self.title = title
+        self.recipe_url = recipe_url
+        self.cuisine = cuisine
+        self.ingredients = list(ingredients)
+        if image_url:
+            self.image_url = image_url
+        if calories:
+            self.calories = calories
+
+    def to_dict(self):
+        dictionary = {
+            'title' : self.title,
+            'recipe_url' : self.recipe_url,
+            'cuisine' : self.cuisine,
+            'ingredients' : self.ingredients
+        }
+        return self.__dict__
+
+r = Recipe('soup','soup.com','vegan',['sugar','spice','everything nice'])
+print(r.to_dict())
