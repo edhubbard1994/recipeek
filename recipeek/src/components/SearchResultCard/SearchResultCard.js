e281acb9c988ac44d3f28072aff44ae1c603343f
Maya <mgocalkappos@luc.edu>
2020-03-23 15:07:59 -0500

    Search result card skeleton.

---
+++ recipeek/src/components/SearchResultCard/SearchResultCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './SearchResultCard.module.css';
+
+const SearchResultCard = (props) => {
+    let {  } = props;
+
+    return (
+
+    );
+};
+
+Searchbar.propTypes = {
+
+};
+
+Searchbar.defaultProps = {
+
+};
+
+export default SearchResultCard;
