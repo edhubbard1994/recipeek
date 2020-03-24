736acf99cb95de075be1664b499f0a4e7e5075ee
Maya <mgocalkappos@luc.edu>
2020-03-23 20:56:51 -0500

    Completed SearchResultCard component.

--- recipeek/src/components/SearchResultCard/SearchResultCard.js
+++ recipeek/src/components/SearchResultCard/SearchResultCard.js
@@ -3,19 +3,48 @@ import PropTypes from 'prop-types';
 import styles from './SearchResultCard.module.css';
 
 const SearchResultCard = (props) => {
-    let {  } = props;
+    let { recipeName, cuisine, diet, recipeURL, recipeImageURL } = props;
 
     return (
-
+      <div onClick={() => {window.open(recipeURL)}} className={styles.card}>
+        <img
+          src={recipeImageURL}
+          alt={recipeName}
+          className={styles.recipeImage}
+        />
+        <h1 className={styles.name}>
+          {recipeName}
+        </h1>
+        <div className={styles.separator}/>
+        <div className={styles.labelBoxCuisine}>
+          <h1 className={styles.label}>Cuisine</h1>
+        </div>
+        <h1 className={styles.cuisineText}>
+          {cuisine}
+        </h1>
+        <div className={styles.labelBoxDiet}>
+          <h1 className={styles.label}>Diet</h1>
+        </div>
+        <h1 className={styles.dietText}>
+          {diet}
+        </h1>
+      </div>
     );
 };
 
-Searchbar.propTypes = {
-
+SearchResultCard.propTypes = {
+  recipeName: PropTypes.string.isRequired,
+  cuisine: PropTypes.string,
+  diet: PropTypes.string,
+  // onClick: PropTypes.func.isRequired,
+  recipeURL: PropTypes.string.isRequired,
+  recipeImageURL: PropTypes.string,
 };
 
-Searchbar.defaultProps = {
-
+SearchResultCard.defaultProps = {
+  cuisine: 'unknown',
+  diet: 'unknown',
+  recipeImageURL: 'https://image.flaticon.com/icons/svg/857/857681.svg',
 };
 
 export default SearchResultCard;
