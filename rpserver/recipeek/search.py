5130e544ad60976d4e353303df5aa2325462266a
Sam Siner <ssiner2@gmail.com>
2020-04-09 16:25:23 -0500

    Add "has:" keyword functionality to search

--- rpserver/recipeek/search.py
+++ rpserver/recipeek/search.py
@@ -5,17 +5,25 @@ import logging
 
 def search_recipes_sorted(keywords):
     final_results_weighted = {}
-    for word in keywords.split():
-        logging.warning(word)
-        results = search(word)
-        logging.warning(results)
-        for recipe in results:
-            if recipe in final_results_weighted.keys():
-                final_results_weighted[recipe] += results[recipe]
-            else:
-                final_results_weighted[recipe] = results[recipe]
-    return sort(final_results_weighted)
+    words_to_filter = []
 
+    for word in keywords.split():
+        if word.startswith("has:"):
+            logging.warning(word)
+            logging.warning(word[4:])
+            words_to_filter.append(word[4:])
+        else:
+            logging.warning(word)
+            results = search(word)
+            for recipe in results:
+                if recipe in final_results_weighted.keys():
+                    final_results_weighted[recipe] += results[recipe]
+                else:
+                    final_results_weighted[recipe] = results[recipe]
+
+    filtered_list = filterByKeywords(words_to_filter, final_results_weighted)
+
+    return sort(filtered_list)
 
 def search(word):
     # ingredients = Ingredient.objects.filter(name__icontains=word)
@@ -24,9 +32,9 @@ def search(word):
     recipes = Recipe.objects.filter(title__icontains=word)
 
     results = {}
-
-    for recipe in recipes:
-        results[recipe] = 1
+    # 
+    # for recipe in recipes:
+    #     results[recipe] = 1
 
     #Increments weight of each recipe by a predetermined amount.
     #Recipe title is weighted higher, and so are multiple matches.
@@ -64,6 +72,20 @@ def search(word):
 
     return results
 
+def filterByKeywords(words_to_filter, final_results_weighted):
+    final_filtered_recipes = final_results_weighted.copy()
+
+    for recipe in final_results_weighted.keys():
+        should_delete = False
+
+        for word in words_to_filter:
+            if word.lower() not in recipe.title.lower():
+                should_delete = True
+
+        if should_delete:
+            final_filtered_recipes.pop(recipe)
+
+    return final_filtered_recipes
 
 def sort(final_results):
     new_dict = OrderedDict(sorted(final_results.items(), key=lambda x: x[1], reverse=True))
