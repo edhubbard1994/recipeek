dc6643ee3715a8132e29331af93a7dd87f3eab98
Maya <mgocalkappos@luc.edu>
2020-04-17 16:47:56 -0500

    Added ProfilePage to routes.

--- recipeek/src/App.js
+++ recipeek/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/styles/App.css';
 import Home from './routes/Home';
+import ProfilePage from './routes/ProfilePage';
 import { Route, Redirect, Switch } from 'react-router-dom'
 
 class App extends React.Component {
@@ -28,6 +29,7 @@ class App extends React.Component {
       <div className="App">
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/profile" component={ProfilePage} />
           <Redirect to="/" />
         </Switch>
       </div>
