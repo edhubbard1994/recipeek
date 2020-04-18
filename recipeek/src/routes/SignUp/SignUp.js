ae99fbcd833e6f4bfb6d92c9104c00d18dda0921
Maya <mgocalkappos@luc.edu>
2020-04-18 12:11:35 -0500

    Completed signup page layout.

--- recipeek/src/routes/SignUp/SignUp.js
+++ recipeek/src/routes/SignUp/SignUp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import GradientButton from '../../components/GradientButton';
 import styles from './SignUp.module.css';
 import logo from '../../assets/recipeek-logo.png';
 
@@ -6,11 +7,53 @@ export default class SearchResults extends Component {
     render() {
         return (
           <div className={styles.wrapper}>
-            <img
-              src={logo}
-              alt={"logo"}
-              className={styles.logo}
-            />
+              <div>
+                <img
+                  src={logo}
+                  alt={"logo"}
+                  className={styles.logo}
+                />
+              </div>
+              <div>
+                  <div className={styles.inputWrap}>
+                    <input
+                        className={styles.inputFields}
+                        type="text"
+                        placeholder={'Username'}
+                        onChange={() => {alert('username!')}}
+                    />
+                  </div>
+                  <div className={styles.inputWrap}>
+                    <input
+                        className={styles.inputFields}
+                        type="text"
+                        placeholder={'email@example.com'}
+                        onChange={() => {alert('email!')}}
+                    />
+                  </div>
+                  <div className={styles.inputWrap}>
+                    <input
+                        className={styles.inputFields}
+                        type="text"
+                        placeholder={'Password'}
+                        onChange={() => {alert('password!')}}
+                    />
+                  </div>
+                  <div className={styles.inputWrap}>
+                    <input
+                        className={styles.inputFields}
+                        type="text"
+                        placeholder={'Confirm Password'}
+                        onChange={() => {alert('confirm pass!')}}
+                    />
+                  </div>
+              </div>
+              <div>
+                <GradientButton
+                  label = {'Sign Up'}
+                  onClick = {() => {alert('clicked!')}}
+                />
+              </div>
           </div>
         );
     }
