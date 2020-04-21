5e484bcc746ac78c3f695b3edf3f96e19bd7c893
Maya <mgocalkappos@luc.edu>
2020-04-21 13:22:16 -0500

    test commit

--- recipeek/src/routes/Home/Home.js
+++ recipeek/src/routes/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Searchbar from '../../components/Searchbar';
 import styles from './Home.module.css';
 import SearchResultCard from '../../components/SearchResultCard';
+import GradientButton from '../../components/GradientButton';
 
 
 
@@ -41,6 +42,12 @@ export default class Home extends Component {
                   onClick = {() => {this.btnCallback(this.state.searchString,this.setState);this.setState({searchString: ""})}}
                   searchInput = ''
                   onChange = { (e) => this.setState({searchString: e.target.value}) }
+                />
+              </div>
+              <div className={styles.button}>
+                <GradientButton
+                  label = {'Advanced'}
+                  onClick = {() => {alert('Keywords:\nhas: to specify required term\ndiet: vegan, vegetarian, paleo, dairy-free, wheat-free, fat-free, low-sugar, egg-free, peanut-free, tree-nut-free, soy-free, fish-free, shellfish-free')}}
                 />
               </div>
             </div>
